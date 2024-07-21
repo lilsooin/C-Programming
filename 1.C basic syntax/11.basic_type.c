f28712753c04d43b7cbd632488f96ff232830b66
sooin <lilesooin@gmail.com>
2024-07-20 23:41:47 -0400

    code sample: basic type

---
+++ 1.C basic syntax/11.basic_type.c
@@ -0,0 +1,16 @@
+#include <stido.h>
+
+int main(void)
+{
+    int int_value = 10;
+    long long_value = 100;
+    float float_value = 1.5f;
+    char char_value = 'A';
+    
+    printf("int_value: %d\n", int_value);
+    printf("long_value: %ld\n", long_value);
+    printf("float_value: %f\n", flaot_value);
+    printf("char_value: %c\n", char_value);
+
+   return 0;
+}
