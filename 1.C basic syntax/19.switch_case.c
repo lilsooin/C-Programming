29c62fa923b6ccfe620b72553af933f9b4f33207
sooin <lilesooin@gmail.com>
2024-07-22 00:47:23 -0400

    19.switch_case.c

---
+++ 1.C basic syntax/19.switch_case.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 3;
+    switch (x) {
+    case 0:
+        printf("Case 0\n");
+        break;
+    case 1:
+        printf("Case 1\n");
+    case 2:
+        printf("Case 2\n");
+        break;
+    case 3:
+        printf("Case 3\n);
+        /* intentional fallthrough */
+    default:
+        printf("Default case\n");
+        break;
+    }
+    
+    return 0;
+}
