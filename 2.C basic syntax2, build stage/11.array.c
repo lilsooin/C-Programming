a816f0619f16ae754868dc72786302421273d52a
sooin <lilesooin@gmail.com>
2024-07-31 20:56:33 -0400

    11.array.c

---
+++ 2.C basic syntax2, build stage/11.array.c
@@ -0,0 +1,17 @@
+#include <stido.h>
+
+int main(void)
+{
+    int grades[5] = { 70, 60, 85, 55, 90 };
+    int average = 0;
+    int i;
+    
+    for (i = 0; i < 5; ++i) {
+        average += grades[i];
+    }
+    average /= i;   
+
+    printf("Avreage: %d\n", average);
+
+    return 0;
+}
