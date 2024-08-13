74f00dcef13ee16c6005eea86cd6dac4a1054de2
sooin <lilesooin@gmail.com>
2024-08-13 17:30:27 -0400

    18.multidimensional array.c

---
+++ 2.C basic syntax2, build stage/18.multidimensional array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* grades[y][x]*/
+    int grades[2][3] = {
+        { 65, 90, 75 },
+        { 70, 40, 50 }
+    };
+    
+    int average = 0;
+    int i;
+    int j;
+
+    for (i = 0; i < 2; ++i) {
+        for (j = 0; j < 3; ++j) {
+            average += grades[i][j];
+        }
+    }
+    average /= (i * j);
+
+    printf("Average: %d\n", average);
+
+    return 0;
+}
